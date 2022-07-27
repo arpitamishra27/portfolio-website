4fec76a70f5242306fd5ba2ed607c2c86a1752b5
Arpita Mishra <arpitamishra2799@gmail.com>
2022-07-27 03:51:16 -0400

    git update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/Vinitaparasrampuria/Portfolio"
+                href="https://github.com/arpitamishra27/portfolio-website"
                 target="_blank"
                 className="fork-btn-inner"
               >
