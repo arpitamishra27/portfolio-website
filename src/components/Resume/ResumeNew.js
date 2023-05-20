a0dc1833000a915d613a338b1aa0a99c128ec17c
Arpita Mishra <arpitamishra2799@gmail.com>
2023-05-20 18:27:47 -0400

    Resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/ARPITA_MISHRA-RESUME-fte.pdf";
+import pdf from "../../Assets/../Assets/ARPITA_MISHRA_RESUME_23fte.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
