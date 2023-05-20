c7ffbfb85364391fc6cd32a5fa61e07012dd566c
Arpita Mishra <arpitamishra2799@gmail.com>
2023-05-20 18:45:33 -0400

    Debugging

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,17 +17,18 @@ function Projects() {
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={hms}
-              isBlog={false}
-              title="Crime Data Analysis, Visualization and Prediction of Crime Events"
-              description="Led a team of 4 people, created user interface using concept of neumorphism and worked on front end.
-              Created a unified platform for crime data visualization, analysis and real-time prediction of future crime trends. 
-              It is advanced as acomplete product and can be applied for any country's dataset.
-               Formed backend through zingcharts, amcharts, anymap, folium and google maps embed 
-               API to conduct data visualization.• Live prediction on dataset is carried out by 
-               time series forecasting with Prophet library of python."
-              ghLink="https://github.com/arpitamishra27/Crime-Data-Analysis"
+          <ProjectCard
+              //imgPath={hms}
+              //isBlog={false}
+             // title="Crime Data Analysis, Visualization and Prediction of Crime Events"
+             // description="Led a team of 4 people, created user interface using concept of neumorphism and worked on front end.
+             // Created a unified platform for crime data visualization, analysis and real-time prediction of future crime trends. 
+            //  It is advanced as acomplete product and can be applied for any country's dataset.
+             //  Formed backend through zingcharts, amcharts, anymap, folium and google maps embed 
+             //  API to conduct data visualization.• Live prediction on dataset is carried out by 
+             //  time series forecasting with Prophet library of python."
+             // ghLink="https://github.com/arpitamishra27/Crime-Data-Analysis"
+      
               
             />
           </Col>
