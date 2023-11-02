02dc7ec58a11aef7028728eb0a2728b2dc8e113d
Arpita Mishra <arpitamishra2799@gmail.com>
2023-11-02 09:27:38 -0400

    Adding the certifications link.

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -19,8 +19,16 @@ function Certification() {
           </p>
           <ul style={{textAlign:"left"}}>
           <li className="certification" >
-              2022: "How to create a Jira SCRUM project" completed on Jul 12, by Coursera
-              <br/>  
+          2022:{" "}
+              <a
+                href="https://coursera.org/share/8003c806475c9c86d5431e6b0a1933bc"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "How to create a Jira SCRUM project"
+              </a>{" "}
+              completed on Jul 12, by Coursera
+              <br />
               <br />
             </li>
           <li className="certification" >
