4c2a932f7b88b624767df2fb93afe22e55b39a4f
Arpita Mishra <arpitamishra2799@gmail.com>
2023-11-02 09:58:02 -0400

    updates

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -6,20 +6,20 @@ function Certification() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "center" }}> 
-          <br />
-          <br />
-          <br />
-          <br />
-                    Below are some of my Certifications!
-                    <br />
-                    <br />
-                    <br />
+          <p style={{ textAlign: "center" }}>
+            <br />
+            <br />
+            <br />
+            <br />
+            Below are some of my Certifications!
+            <br />
+            <br />
+            <br />
 
           </p>
-          <ul style={{textAlign:"left"}}>
-          <li className="certification" >
-          2022:{" "}
+          <ul style={{ textAlign: "left" }}>
+            <li className="certification" >
+              2022:{" "}
               <a
                 href="https://coursera.org/share/8003c806475c9c86d5431e6b0a1933bc"
                 target="_blank"
@@ -27,27 +27,134 @@ function Certification() {
               >
                 "How to create a Jira SCRUM project"
               </a>{" "}
-              completed on Jul 12, by Coursera
+              by Coursera.
               <br />
               <br />
-            </li>
-          <li className="certification" >
-              2022: "Git Essential Training: The Basics" completed on Jan 03, by LinkedIn Learning
-                    <br/> 
-                    <br />
             </li>
 
-            <li className="certification" >
-              2022: "Learning SQL Programming" completed on Jan 06, by LinkedIn Learning
-              <br/> 
+            <li className="certification">
+              2022:{" "}
+              <a
+                href="https://www.linkedin.com/learning/certificates/7fb4bda704b89f578f3f82185c9ada77ec0eca765cdf291b194f9460644afd19?u=2131553"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "Learning REST APIs"
+              </a>{" "}
+              by LinkedIn Learning.
+              <br />
               <br />
             </li>
-                  
+
+            <li className="certification">
+              2022:{" "}
+              <a
+                href="https://coursera.org/share/369a5d82ddf784253d40bf62b6672e40"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "AWS S3 Basics"
+              </a>{" "}
+              by Coursera.
+              <br />
+              <br />
+            </li>
+
+        
+
+            <li className="certification">
+              2022:{" "}
+              <a
+                href="https://www.coursera.org/account/accomplishments/certificate/ZA5TGPJHKHJZ"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "Essential Google Cloud Infrastructure: Foundation"
+              </a>{" "}
+              by Coursera.
+              <br />
+              <br />
+            </li>
+
+            <li className="certification">
+              2022:{" "}
+              <a
+                href="https://coursera.org/share/6e88589fea9faa5b87c59fbc4cbd9e71"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "Reliable Google Cloud Infrastructure: Design and Process"
+              </a>{" "}
+              by Coursera.
+              <br />
+              <br />
+            </li>
+
+            <li className="certification">
+              2022:{" "}
+              <a
+                href="https://coursera.org/share/ef4c7bfc47478fd61ae6be297945e743"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "Essential Google Cloud Infrastructure: Core Services"
+              </a>{" "}
+              by Coursera.
+              <br />
+              <br />
+            </li>
+
+            <li className="certification">
+              2022:{" "}
+              <a
+                href="https://coursera.org/share/c19687cc297eeb971ed0db7694be7f9b"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "Elastic Google Cloud Infrastructure: Scaling and Automation"
+              </a>{" "}
+              by Coursera.
+              <br />
+              <br />
+            </li>
+
+            <li className="certification">
+              2022:{" "}
+              <a
+                href="https://coursera.org/share/f892c6b9e840733c084f9afdbbee402b"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "Google Cloud Fundamentals: Core Infrastructure"
+              </a>{" "}
+              by Coursera.
+              <br />
+              <br />
+            </li>
+
+            <li className="certification">
+              2022:{" "}
+              <a
+                href="https://drive.google.com/file/d/1-VGgkDkrvnsf3hC0cvatkQXDkZgrpNXF/view"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                "FUNDAMENTALS OF DEEP LEARNING FOR COMPUTER VISION"
+              </a>{" "}
+              by NVIDIA Deep Learning Institute.
+              <br />
+              <br />
+            </li>
+
+
+
+
+
           </ul>
           <br />
           <br />
           <br />
-          
+
         </blockquote>
       </Card.Body>
     </Card>
